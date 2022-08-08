d301cc3fa00468c0fe999809436ed2206199a788
CainHarniess <cainharniess@gmail.com>
2022-08-08 22:20:54 +0100

    Fixed vocab info query projection.

---
+++ GermanVocabApp.DataAccess.EntityFramework/Projection/VocabListItemProjectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using GermanVocabApp.DataAccess.EntityFramework.Models;
+using GermanVocabApp.DataAccess.Shared.DataTransfer;
+
+namespace GermanVocabApp.DataAccess.EntityFramework.Projection;
+
+public static class VocabListItemProjectionExtensions
+{
+    public static IQueryable<VocabListInfoDto> ProjectToInfoDto(this IQueryable<VocabList> query)
+    {
+        IQueryable<VocabListInfoDto> infoQuery;
+        infoQuery = query.Select(vl => new VocabListInfoDto()
+        {
+            Id = vl.Id,
+            Name = vl.Name,
+            Description = vl.Description,
+        });
+        return infoQuery;
+    }
+}
