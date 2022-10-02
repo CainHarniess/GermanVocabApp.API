be9dfb98a5f06f559b84f9caf538408ddab10eb4
CainHarniess <cainharniess@gmail.com>
2022-10-02 15:28:00 +0100

    Fixed formatting in the vocab list DB context.

--- GermanVocabApp.DataAccess.EntityFramework/VocabListDbContext.cs
+++ GermanVocabApp.DataAccess.EntityFramework/VocabListDbContext.cs
@@ -5,8 +5,6 @@ using Microsoft.EntityFrameworkCore;
 namespace GermanVocabApp.DataAccess.EntityFramework;
 public class VocabListDbContext : DbContext
 {
-    //public const string ConnectionString = "Data Source= (localdb)\\MSSQLLocalDB; Initial Catalog=GermanVocabApp";
-
     public VocabListDbContext(DbContextOptions options) : base(options)
     {
 
@@ -14,12 +12,6 @@ public class VocabListDbContext : DbContext
 
     public DbSet<VocabList> VocablLists { get; set; }
     public DbSet<VocabListItem> VocablListItems { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        base.OnConfiguring(optionsBuilder);
-
-    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
