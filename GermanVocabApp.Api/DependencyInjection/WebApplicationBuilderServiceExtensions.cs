d65eb950a1dab74f81d09e4ffca28734fe85715e
CainHarniess <cainharniess@gmail.com>
2022-12-18 15:38:53 +0000

    Added security configuration to the swagger generation.

--- GermanVocabApp.Api/DependencyInjection/WebApplicationBuilderServiceExtensions.cs
+++ GermanVocabApp.Api/DependencyInjection/WebApplicationBuilderServiceExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using GermanVocabApp.Api.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 namespace GermanVocabApp.Api.DependencyInjection;
@@ -28,6 +29,37 @@ public static class WebApplicationBuilderServiceExtensions
                 ValidAudience = AuthenticationConstants.Audience,
                 IssuerSigningKey = new SymmetricSecurityKey(secretBytes),
             };
+        });
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddSwaggerConfiguration(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSwaggerGen(o =>
+        {
+            o.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "bearer"
+            });
+            o.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type=ReferenceType.SecurityScheme,
+                            Id="Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
         });
         return builder;
     }
--- GermanVocabApp.Api/Program.cs
+++ GermanVocabApp.Api/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddControllers()
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.AddSwaggerConfiguration();
 
 builder.Services.AddEntityFrameworkDependencies(builder);
 builder.Services.AddValidationDependencies();
